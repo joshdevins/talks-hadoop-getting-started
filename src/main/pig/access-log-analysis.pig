df55230cfc76d042925ac58dc19582352186f296
Josh Devins <info@joshdevins.net>
2011-03-22 17:15:24 +0100

    Added PigUnit test for access-log-analysis.pig

--- src/main/pig/access-log-analysis.pig
+++ src/main/pig/access-log-analysis.pig
@@ -1,16 +1,14 @@
 set job.name 'Apache Access Log Analysis'
 set default_parallel 2
 
-REGISTER target/hadoop-getting-started-1-SNAPSHOT-jar-with-dependencies.jar
-
 -- PiggyBank UDFs
 DEFINE ApacheCombinedLogLoader org.apache.pig.piggybank.storage.apachelog.CombinedLogLoader();
 
-logs = LOAD 'data/logs/*-access.log' USING ApacheCombinedLogLoader AS
+raw = LOAD 'data/logs/*-access.log' USING ApacheCombinedLogLoader AS
     (remoteAddr, remoteLogname, user, timestamp, method, uri, protocol, statusCode, bytes, referrer, userAgent);
 
 -- project just the fields that we want
-logs = FOREACH logs GENERATE method, uri, statusCode;
+logs = FOREACH raw GENERATE method, uri, statusCode;
 
 -- determine the types of requests/responses that appear in the logs
 -- group by all fields to find and count all permutations
@@ -20,5 +18,7 @@ grouped = GROUP logs BY (method, uri, statusCode);
 permutations = FOREACH grouped GENERATE
     group.method AS method, group.uri AS uri, group.statusCode AS statusCode,
     COUNT(logs) AS count;
+    
+permutations = ORDER permutations BY count DESC;
     
 STORE permutations INTO 'access-log-analysis' USING PigStorage(',');
---
+++ src/test/java/net/joshdevins/talks/hadoopstart/pig/scripts/AccessLogAnalysisTest.java
@@ -0,0 +1,23 @@
+package net.joshdevins.talks.hadoopstart.pig.scripts;
+
+import org.apache.pig.pigunit.PigTest;
+import org.junit.Test;
+
+public final class AccessLogAnalysisTest {
+
+    @Test
+    public void testScript() throws Exception {
+
+        PigTest test = new PigTest("src/main/pig/access-log-analysis.pig");
+
+        String[] input = {
+                "1.2.3.4\t-\t-\t30/Sep/2008:15:07:53 -0400\tGET\t/\tHTTP/1.1\t200\t3190\t-\tMozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_4; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1",
+                "1.2.3.4\t-\t-\t30/Sep/2008:15:07:53 -0400\tGET\t/\tHTTP/1.1\t200\t3190\t-\tMozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_4; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1",
+                "1.2.3.4\t-\t-\t30/Sep/2008:15:07:53 -0400\tGET\t/foo\tHTTP/1.1\t404\t3190\t-\t-" };
+        String[] output = { "(GET,/,200,2)", "(GET,/foo,404,1)" };
+
+        // PigUnit will discard any STORE and DUMP calls
+        // PigUnit will replace input tuple with one loaded and parsed with PigStorage
+        test.assertOutput("raw", input, "permutations", output);
+    }
+}
